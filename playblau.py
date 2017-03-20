7e6822612c2471785391c419c0f2c1e9d66a2772
anikom95 <aniko.milz@web.de>
2017-03-20 13:10:42 +0100

    added files

---
+++ playblau.py
@@ -0,0 +1,35 @@
+import pyaudio
+import wave
+import sys
+import time
+
+class AudioFile:
+    chunk = 1024
+
+    def __init__(self, file):
+        """ Init audio stream """
+        self.wf = wave.open(file, 'rb')
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(
+            format = self.p.get_format_from_width(self.wf.getsampwidth()),
+            channels = self.wf.getnchannels(),
+            rate = self.wf.getframerate(),
+            output = True
+        )
+
+    time.sleep(6)
+    def play(self):
+        """ Play entire file """
+        data = self.wf.readframes(self.chunk)
+        while data != '':
+            self.stream.write(data)
+            data = self.wf.readframes(self.chunk)
+
+    def close(self):
+        self.stream.close()
+        self.p.terminate()
+
+# Usage example for pyaudio
+a = AudioFile("blau.wav")
+a.play()
+a.close()
